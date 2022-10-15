9bb73796b30180f2d145f4504c9af3d66d8e5f95
mukul <mukul@192.168.1.41>
2022-10-15 16:42:11 +0530

    update a program

--- Practice_Questions_on_Java/src/Questions/ReverseaString.java
+++ Practice_Questions_on_Java/src/Questions/ReverseaString.java
@@ -3,27 +3,44 @@ package Questions;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 // String bulilder class does have a reverse function
 public class ReverseaString {
 
 	public static void main(String[] args) {
 		
-  String str= "Hello my name is mukul";
+  String st= "Hello my name is mukul";
+	String reverse=reverseStringUsingStack(st);
+  String rverseString =reverse(st);
+// System.out.println(rverseString);
+  System.out.println(reverse);
 	/*
-	 * char[] chtr =str.toCharArray(); char[] rev= new char[chtr.length]; for(int i=
-	 * (chtr.length-1);i>0;i--) {
-	 * 
-	 * rev[] =chtr[i];
-	 * 
-	 * }
+	 * int len = str.length(); String rev=""; for (int i =len-1;i>=0;i--) {
+	 * rev=rev+str.charAt(i); } System.out.println(rev);
 	 */
-	/* StringBuilder stbr = new StringBuilder(str); */
-  int len = str.length();
-  String rev="";
-  for (int i =len-1;i>=0;i--) {
-	  rev=rev+str.charAt(i);
-  }
-  System.out.println(rev);
 	}
-
+static String reverse(String st) {
+	String str =st;
+	String rev = "";
+	int strln=str.length();
+	while(strln>0) {
+		rev=rev+ str.charAt(strln-1);
+		strln--;
+	}
+	
+	return rev;
+}
+	
+static String reverseStringUsingStack(String str) {
+	String rev ="";
+	str.replaceAll("//s", "");	
+	Stack<String> stackList =new Stack<String>();	
+		stackList.add(str);
+		while(stackList.size()>0) {
+			rev = rev+stackList.peek();
+			stackList.pop();
+		}	
+	return rev;
+	
+}
 }
